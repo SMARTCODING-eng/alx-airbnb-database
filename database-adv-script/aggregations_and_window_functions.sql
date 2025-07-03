395e480582328b7b9e88220abd044403963e13f9
Elutayo <elutayojohnson@gmail.com>
2025-07-03 08:30:42 +0100

    modify

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -25,3 +25,28 @@ FROM (
 ) AS PropertyBookings
 ORDER BY
     booking_rank ASC, property_title ASC;
+
+
+
+
+
+    SELECT
+    property_id,
+    property_title,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC, property_id ASC) AS booking_row_num
+FROM (
+    SELECT
+        p.property_id,
+        p.title AS property_title,
+        COUNT(b.booking_id) AS total_bookings
+    FROM
+        properties AS p
+    LEFT JOIN
+        bookings AS b ON p.property_id = b.property_id
+    GROUP BY
+        p.property_id,
+        p.title
+) AS PropertyBookings
+ORDER BY
+    booking_row_num ASC;

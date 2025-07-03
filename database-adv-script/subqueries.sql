d2543d6ab8d7fe6c58e59e4a683c6531a235f859
Elutayo <elutayojohnson@gmail.com>
2025-07-03 04:16:40 +0100

    Modify subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,4 +1,8 @@
 SELECT property_id, title, price FROM properties as p
 SELECT property_id FROM reviews AS r 
 WHERE r.property_id = p.property_id 
-HAVING AVG(r.rating) > 4.0
+HAVING AVG(r.rating) > 4.0;
+
+
+SELECT * FROM users as u 
+(SELECT COUNT(b.booking_id) AS b WHERE b.user_id = u.user_id) > 3 

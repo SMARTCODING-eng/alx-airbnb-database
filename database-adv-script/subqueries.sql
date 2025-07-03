e5a203d5ef55072ec8b53cc6aa83c8023ca4ebe6
Elutayo <elutayojohnson@gmail.com>
2025-07-03 04:09:50 +0100

    Good

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,3 +1,4 @@
 SELECT property_id, title, price FROM properties as p
 SELECT property_id FROM reviews AS r 
-WHERE r.property_id = p.property_id AND r.rating > 4.0
+WHERE r.property_id = p.property_id 
+HAVING AVG(r.rating) > 4.0
